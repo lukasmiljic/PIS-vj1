24c4277608b98a7ab1ec6c67deb72d194ea9d45b
ancovic <antcov0101@gmai.com>
2025-03-17 18:55:05 +0100

    Added new class NewClass1

---
+++ PIS_vj1/NewClass1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_vj1
+{
+    internal class NewClass1
+    {
+        static void Greet()
+        {
+            Console.WriteLine("Hi! Hellooo! :DDD");
+        }
+
+        static void Conflict()
+        {
+            Console.WriteLine("CONFLICT!!!");
+        }
+
+        static void Introduce()
+        {
+            Console.WriteLine("I'm a new class!");
+        }
+    }
+}
