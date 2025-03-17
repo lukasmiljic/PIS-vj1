52333c9b5dcbbf02addfaf20f83dce41a8939540
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-17 18:18:47 +0100

    Add new class

---
+++ PIS_vj1/NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_vj1
+{
+    internal class NewClass
+    {
+        static void Greet()
+        {
+            Console.WriteLine("Hi! Hellooo! :DDD");
+        }
+    }
+}
